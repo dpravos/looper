17ec67cef8c159e7fd85717048cf6fefe2238f9b
dpravos <dpravos@gmail.com>
2022-10-05 19:32:33 +0200

    Initial Commit

---
+++ src/main/java/com/lambdaloopers/looper/LooperApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaloopers.looper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LooperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LooperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambdaloopers/looper/LooperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaloopers.looper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
